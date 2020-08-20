a16e5bcd13676a8dcd0ed9e27a931c4a60efdd61
Izabela Rojek <izabela_rojek@op.pl>
2020-08-20 12:07:39 +0200

    Add basic html, functions.js, variables in script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'kamień';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+ 
