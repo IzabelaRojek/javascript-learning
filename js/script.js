d6e86d9d584894aa0e14922542badeac5d28f4bd
Izabela Rojek <izabela_rojek@op.pl>
2020-09-07 23:34:02 +0200

    Change var to let, add new constants

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 const stone= 'kamień';
 const scissors = 'nożyce';
 const paper = 'papier';
+const win = 'Wygrywasz!';
+const loss = 'Przegrywasz :(';
+const tie = 'Remis!';
 
 function buttonClicked(argButtonName) {
   clearMessages();
   console.log(argButtonName + ' został kliknięty');
 
-  var argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
+  let argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber;
 
-  /**
-   * Describe this function...
-   */
   function getMoveName(argMoveId) {
     console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
     if (argMoveId == 1) {
@@ -19,32 +19,25 @@ function buttonClicked(argButtonName) {
       return paper;
     } else if (argMoveId == 3) {
       return scissors;
-    } else {
-      printMessage('Nie znam ruchu o id ' + argMoveId + '. Zakładam, że chodziło o "kamień".');
-      return stone;
     }
   }
 
-  /**
-   * Describe this function...
-   */
   function displayResult(argPlayerMove, argComputerMove) {
     console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
     if (argPlayerMove == paper && argComputerMove == stone) {
-      printMessage('Wygrywasz!');
+      printMessage(win);
     } else if (argPlayerMove == stone && argComputerMove == scissors) {
-      printMessage('Wygrywasz!');
+      printMessage(win);
     } else if (argPlayerMove == scissors && argComputerMove == paper) {
-      printMessage('Wygrywasz!');
+      printMessage(win);
     } else if (argPlayerMove == argComputerMove) {
-      printMessage('Remis!');
+      printMessage(tie);
     } else {
-      printMessage('Przegrywasz :(');
+      printMessage(loss);
     }
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
   }
 
-  console.log('wybór ruchu gracza to: ' + playerInput);
   playerMove = argButtonName;
   console.log('ruch gracza to: ' + playerMove);
   randomNumber = Math.floor(Math.random() * 3 + 1);
